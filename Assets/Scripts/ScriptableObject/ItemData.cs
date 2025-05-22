cdd57bc094a3284a49e6095f9e2246c394979d54
BAOBAB2231 <136507108+BAOBAB2231@users.noreply.github.com>
2025-05-23 06:49:32 +0900

    플레이어가 바라보는 오브젝트의 정보를 표시
    - 화면의 정중앙에 에임을 만들어 바라보는 방향을 명확히 함
    - 아이템 데이터를 ScriptableObject로 정의. 각 아이템의 이름, 설명, 속성 등을 ScriptableObject로 관리
    - Raycast를 통해 플레이어가 바라보는 오브젝트의 정보를 UI에 표시
    - 아이템을 가르치고 있지 않을 땐, 다시 아이템 정보 UI를 닫음

--- Assets/Scripts/ScriptableObject/ItemData.cs
+++ Assets/Scripts/ScriptableObject/ItemData.cs
@@ -1,19 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+public enum ConsumableType
+{
+    Health,
+    SpeedUp
+}
 
+[Serializable]
+public class ItemDataConsumbale
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
 public class ItemData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Info")]
+    public string displayName;
+    public string description;
+    public GameObject dropPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Consumable")]
+    public ItemDataConsumbale[] consumables;
 }
